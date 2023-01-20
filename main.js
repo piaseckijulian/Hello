9a5104f04dbeff4bc4e2d59cbb6dd5a3051d6ad0
Julian Piasecki <julian.piasecki@outlook.com>
2023-01-21 00:22:19 +0100

    Fixing Location Issue

--- main.js
+++ main.js
@@ -2,47 +2,50 @@
 const helloDiv = document.getElementById('hello');
 
 /**
- * It fetches the user's IP address, language code, and country name from the ipapi.co API, and then
- * passes those values to the getMessage function.
+ * It's an async function that fetches data from an API, parses it, and then passes it to another
+ * function.
  */
 const getData = async () => {
-  const url = 'https://ipapi.co/json/';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '51f74c4f0cmsh9a84f657a6708cap1bb793jsn44041051020c',
+      'X-RapidAPI-Host':
+        'find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com',
+    },
+  };
 
-  const response = await fetch(url);
+  const url =
+    'https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation?apikey=873dbe322aea47f89dcf729dcc8f60e8';
+
+  const response = await fetch(url, options);
   const data = await response.json();
 
-  const langCode = data.languages;
+  const lang = data.languages.slice(0, 2);
+  const country = data.country;
+  const ip = data.ip;
 
-  let lang = '';
-  for (const letter of langCode) {
-    if (lang.length >= 2) {
-      break;
-    } else {
-      lang += letter;
-    }
-  }
-  const countryName = data.country_name;
-  const userIP = data.ip;
-
-  getMessage(lang, userIP, countryName);
+  getMessage(lang, ip, country);
 };
 
 /**
- * It takes three arguments, makes a request to the API, and then displays the result in the DOM
- * @param lang - The language code of the language you want to display hello.
+ * It takes a language, an IP address, and a country name, and then it uses the language and IP address
+ * to get a greeting from the API, and then it displays the greeting in the country name
+ * @param lang - The language code of the language you want to get the greeting for.
  * @param ip - The IP address of the user.
- * @param country - The country of the user.
+ * @param country - The country code of the user's location.
  */
 const getMessage = async (lang, ip, country) => {
-  const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-  const BASE_URL = `https://stefanbohacek.com/hellosalut/?lang=${lang}&ip=${ip}`;
+  const proxy = 'https://api.allorigins.win/get?url=';
+  const url = `https://www.stefanbohacek.com/hellosalut/?lang=${lang}&ip=${ip}`;
 
-  const response = await fetch(PROXY_URL + BASE_URL);
+  const response = await fetch(`${proxy}${encodeURIComponent(`${url}`)}`);
   const data = await response.json();
 
-  const hello = data.hello;
+  const hello = JSON.parse(data.contents).hello;
 
   helloDiv.innerHTML = `In <b>${country}</b> we say <b>${hello}</b>!`;
 };
 
+/* It's calling the getData function when the window loads. */
 window.onload = getData;
